e17008f1e75cd075a30ed940b0044d86d73d3c21
CW046360 <colin.ward3@cerner.com>
2016-06-19 00:40:38 -0500

    Colin - committed to pushing to the limit

--- grails-app/controllers/crowdsound/UserController.groovy
+++ grails-app/controllers/crowdsound/UserController.groovy
@@ -4,6 +4,14 @@ class UserController {
 
     def scaffold = crowdsound.User
     def index() { }
-    def partyview() {}
+    def partyview() {
+        String code = "!TODO!"
+        String nickname = params.nickname
+        String artists = params.a1 + ", " + params.a2 + ", " + params.a3 + ", " + params.a4 + ", " + params.a5
+        String genres = params.g1 + ", " + params.g2 + ", " + params.g3 + ", " + params.g4 + ", " + params.g5
+        int admin = 0
+        User u = new User(code, nickname, artists, genres, admin)
+        u.save()
+    }
     def adminview() {}
 }
