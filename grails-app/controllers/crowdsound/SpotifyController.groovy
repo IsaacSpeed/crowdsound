a0d25c7543eaf43fdd2e4c7d88f8f6a06daca302
IsaacSpeed <ies4466@truman.edu>
2016-06-19 13:05:30 -0500

    Isaac - Added action to SpotifyController to generate/add song to playlist, given a party code

--- grails-app/controllers/crowdsound/SpotifyController.groovy
+++ grails-app/controllers/crowdsound/SpotifyController.groovy
@@ -9,20 +9,6 @@ class SpotifyController {
 
     def index() { render "this is an index fam." }
 
-    def addCoolSongsToNewPlaylist() {
-        Auth userAuth = Auth.findByUserId("12182647490")
-        String accessToken = userAuth.authorize()
-
-        SpotifyWrapper wrapper = new SpotifyWrapper()
-        wrapper.setAccessToken(accessToken)
-
-        wrapper.createPlaylist(userAuth.userId, "My cool playlist yo")
-
-        String messages = "$userAuth.userId and $accessToken"
-
-        return [playlistName: wrapper.getPlaylistByName(userAuth.userId, "My cool playlist yo").getName(), messages: messages]
-    }
-
     def showRecs() {
         Auth userAuth = Auth.findByUserId("12182647490")
         String accessToken = userAuth.authorize()
---
+++ grails-app/views/spotify/generateSongToPlaylist.gsp
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+    <title>CrowdSound</title>
+    <meta name="layout" content="main">
+    <asset:stylesheet src="main.css" />
+</head>
+
+<body>
+    <h1>${message}</h1>
+</body>
+</html>
