ec7fcc931701dd9d2b23074c3490e18169c8aad2
IsaacSpeed <ies4466@truman.edu>
2016-06-18 19:25:41 -0500

    Isaac - Added method to wrapper to find a playlist by name, & added page to create a playlist

--- grails-app/controllers/crowdsound/SpotifyController.groovy
+++ grails-app/controllers/crowdsound/SpotifyController.groovy
@@ -2,15 +2,18 @@ package crowdsound
 
 class SpotifyController {
 
-    def index() { }
+    def index() { render "this is an index fam" }
 
     def addCoolSongsToNewPlaylist() {
         Auth userAuth = Auth.findByUserId("12182647490")
         String accessToken = userAuth.authorize()
 
         SpotifyWrapper wrapper = new SpotifyWrapper()
-        wrapper.createPlaylist(userAuth.userId, "My cool playlist yo")
+        boolean success = wrapper.createPlaylist(userAuth.userId, "My cool playlist yo")
 
-        [playlistName: wrapper.getPlaylistByName(userAuth.userId, "My cool playlist yo")]
+        if (success)
+            return [playlistName: wrapper.getPlaylistByName(userAuth.userId, "My cool playlist yo")]
+        else
+            return [playlistName: "Failure haha"]
     }
 }
---
+++ grails-app/views/spotify/addCoolSongsToNewPlaylist.gsp
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+    <title>CrowdSound</title>
+    <meta name="layout" content="main">
+    <asset:stylesheet src="main.css" />
+</head>
+
+<body>
+    <h1>You just created a playlist called ${playlistName}</h1>
+</body>
+</html>
