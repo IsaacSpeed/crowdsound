6bb3f4a993a34392a4879272a1b403fee09afbec
IsaacSpeed <ies4466@truman.edu>
2016-06-20 16:43:22 -0500

    Isaac - fixed javascript buttons

--- grails-app/controllers/crowdsound/UserController.groovy
+++ grails-app/controllers/crowdsound/UserController.groovy
@@ -20,11 +20,11 @@ class UserController {
             wrapper.setAccessToken(Auth.findByPartyCode(party.getCode()).authorize())
 
             String a1Id, a2Id, a3Id, a4Id, a5Id
-            if (params.a1) a1Id = wrapper.getFirstArtistResultByName(params.a1).getId()
-            if (params.a2) a2Id = wrapper.getFirstArtistResultByName(params.a2).getId()
-            if (params.a3) a3Id = wrapper.getFirstArtistResultByName(params.a3).getId()
-            if (params.a4) a4Id = wrapper.getFirstArtistResultByName(params.a4).getId()
-            if (params.a5) a5Id = wrapper.getFirstArtistResultByName(params.a5).getId()
+            if (params.a1) a1Id = wrapper.getFirstArtistResultByName(params.a1)?.getId()
+            if (params.a2) a2Id = wrapper.getFirstArtistResultByName(params.a2)?.getId()
+            if (params.a3) a3Id = wrapper.getFirstArtistResultByName(params.a3)?.getId()
+            if (params.a4) a4Id = wrapper.getFirstArtistResultByName(params.a4)?.getId()
+            if (params.a5) a5Id = wrapper.getFirstArtistResultByName(params.a5)?.getId()
 
             if (a1Id) party.addArtist(a1Id)
             if (a2Id) party.addArtist(a2Id)
@@ -40,7 +40,7 @@ class UserController {
 
             party.save()
 
-            Auth auth = Auth.findByPartyCode(partyCode)
+            Auth auth = Auth.findByPartyCode(party.code)
 
             return [ partyCode: party.getCode(), userId: auth.userId, playlistId: party.playlistId, artists: artistNames, genres: party.genres ]
         } else {
@@ -56,11 +56,11 @@ class UserController {
             wrapper.setAccessToken(Auth.findByPartyCode(party.getCode()).authorize())
 
             String a1Id, a2Id, a3Id, a4Id, a5Id
-            if (params.artist1) a1Id = wrapper.getFirstArtistResultByName(params.artist1).getId()
-            if (params.artist2) a2Id = wrapper.getFirstArtistResultByName(params.artist2).getId()
-            if (params.artist3) a3Id = wrapper.getFirstArtistResultByName(params.artist3).getId()
-            if (params.artist4) a4Id = wrapper.getFirstArtistResultByName(params.artist4).getId()
-            if (params.artist5) a5Id = wrapper.getFirstArtistResultByName(params.artist5).getId()
+            if (params.artist1) a1Id = wrapper.getFirstArtistResultByName(params.artist1)?.getId()
+            if (params.artist2) a2Id = wrapper.getFirstArtistResultByName(params.artist2)?.getId()
+            if (params.artist3) a3Id = wrapper.getFirstArtistResultByName(params.artist3)?.getId()
+            if (params.artist4) a4Id = wrapper.getFirstArtistResultByName(params.artist4)?.getId()
+            if (params.artist5) a5Id = wrapper.getFirstArtistResultByName(params.artist5)?.getId()
 
             if (a1Id) party.addArtist(a1Id)
             if (a2Id) party.addArtist(a2Id)
